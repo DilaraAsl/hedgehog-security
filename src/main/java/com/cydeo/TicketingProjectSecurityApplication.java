5eba1cede5818d6ca6ef944fbcb950d9ac35452d
Dilara <dcavus@gmail.com>
2023-03-09 15:18:50 -0800

    security is added

--- src/main/java/com/cydeo/TicketingProjectSecurityApplication.java
+++ src/main/java/com/cydeo/TicketingProjectSecurityApplication.java
@@ -4,6 +4,8 @@ import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication  //this includes @Configuration
 public class TicketingProjectSecurityApplication {
@@ -16,6 +18,11 @@ public class TicketingProjectSecurityApplication {
     @Bean
     public ModelMapper mapper(){
         return new ModelMapper();
+    }
+    // override the password, we have our own password in the database we need to use the methods of this class to endcode our passwords
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder(); // BCryptPasswordEncoder is the implementation
     }
 
 }
